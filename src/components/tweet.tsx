94083742903d27116280b4ee7a3bb0298ff83d03
BingBong <bingbong9981@gmail.com>
2024-03-22 18:59:27 +0900

    Feat : editing tweets

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,8 +1,9 @@
 import { styled } from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -59,8 +60,31 @@ const EditButton = styled.button`
   cursor: pointer;
 `;
 
+const TextArea = styled.textarea`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 0.5px solid white;
+  font-size: 16px;
+  color: white;
+  background-color: rgba(255, 255, 255, 0.15);
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
   const user = auth.currentUser;
+  const [isEditing, setEditing] = useState(false);
+  const [editedTweet, setEditedTweet] = useState("");
 
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweet?");
@@ -73,23 +97,43 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
       }
     } catch (e) {
       console.log(e);
-    } finally {
-      //
     }
   };
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditedTweet(e.target.value);
+  };
   const onEdit = async () => {
-    //
+    const ok = confirm("Are you sure you want to edit this tweet?");
+    if (!ok || user?.uid !== userId) return;
+    try {
+      await updateDoc(doc(db, "tweets", id), {
+        tweet: editedTweet,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+    setEditing(false);
   };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
+        {isEditing ? (
+          <TextArea defaultValue={tweet} onChange={onChange} />
+        ) : (
+          <Payload>{tweet}</Payload>
+        )}
+
         {user?.uid === userId ? (
-          <EditButton onClick={onEdit}>Edit</EditButton>
-        ) : null}
-        {user?.uid === userId ? (
-          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          <>
+            {isEditing ? (
+              <EditButton onClick={onEdit}>Done</EditButton>
+            ) : (
+              <EditButton onClick={() => setEditing(true)}>Edit</EditButton>
+            )}
+
+            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          </>
         ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
